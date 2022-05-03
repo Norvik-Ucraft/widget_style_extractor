dabea5923781cb2acab2a3528340640c4e0dd65a
Norvik Alexian <norvik@ucraft.com>
2022-05-03 18:06:55 +0400

    added new method to find widgets using provided coordinates

--- style_extractor.py
+++ style_extractor.py
@@ -1,4 +1,3 @@
-import os
 import re
 import detect
 import config
@@ -32,3 +31,11 @@ class StyleExtractor:
         self.driver.get(opt.input_url)
 
         return image_shape
+
+    def get_computed_style(self):
+        image_size = self.widget_coordinates()
+        self.driver.set_window_size(image_size[1], image_size[0])
+
+        for coordinates_pair in self.coordinate_list:
+            element = self.driver.execute_script("return document.elementFromPoint(arguments[0], arguments[1])",
+                                                 coordinates_pair[0], coordinates_pair[1])
