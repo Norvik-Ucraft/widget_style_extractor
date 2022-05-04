84ab6cb88cd9a5d49eaa01e413028c9aaada1ebd
Norvik Alexian <norvik@ucraft.com>
2022-05-04 13:05:30 +0400

    get computed sytle and remove extra css property and add dictionaries to a list

--- style_extractor.py
+++ style_extractor.py
@@ -39,3 +39,19 @@ class StyleExtractor:
         for coordinates_pair in self.coordinate_list:
             element = self.driver.execute_script("return document.elementFromPoint(arguments[0], arguments[1])",
                                                  coordinates_pair[0], coordinates_pair[1])
+            if element:
+                css_property = self.driver.execute_script("return window.getComputedStyle(arguments[0], null);", element)
+                keys = list(css_property.keys())[:]
+                temp_computed_style = {}
+                for key in keys:
+                    key_number = re.findall(config.KEY_PATTERN, key)
+                    uppercase_selector = re.findall(config.UPPERCASE_PATTERN, key)
+                    extra_selector = re.findall(config.EXTRA_PATTERN, key)
+
+                    if not key_number and not uppercase_selector and not extra_selector:
+                        temp_computed_style[key] = css_property[key]
+
+                self.computed_style.append(temp_computed_style)
+
+        self.driver.quit()
+        return self.computed_style
